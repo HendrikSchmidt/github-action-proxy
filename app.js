438337053ae0a4271ea0682d11c69af131fbf90f
Hendrik Schmidt <hendriknas@gmail.com>
2022-10-02 18:44:32 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+import { env } from 'node:process';
+
+
+const http = require('node:http');
+const https = require('node:https');
+
+const hostname = env.ENV == 'production' ? env.PROXY_HOST : '127.0.0.1';
+const port = env.ENV == 'production' ? env.PROXY_PORT : 3000;
+
+const server = http.createServer((req, res) => {
+    const postData = JSON.stringify({
+        event_type: 'deploy-prod',
+    });
+
+    const postOptions = {
+        host: 'api.github.com',
+        port: '443',
+        path: env.GITHUB_PATH,
+        method: 'POST',
+        headers: {
+            'accept': 'application/vnd.github.v3+json',
+            'authorization': env.GITHUB_TOKEN,
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(postData),
+            'User-Agent': req.headers['user-agent'],
+        }
+    };
+        
+    const GitHubRequest = https.request(postOptions, res => {
+        res.setEncoding('utf8');
+        res.on('data', chunk => {
+            console.log('Response: ' + chunk);
+        });
+    });
+    GitHubRequest.write(postData);
+    GitHubRequest.on('error', console.error);
+    GitHubRequest.end();
+
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Request proxied.');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at ${hostname}:${port}/`);
+});
